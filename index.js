242ec88b790888d3871778691a8d1d33dd31121b
Davide Zandonella <davide.zandonella@deltatre.com>
2022-05-27 16:49:04 +0200

    solo 1 mangiamo al giorno!

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const questions = require('./questions.json');
 const TelegramBot =require('node-telegram-bot-api')
 const bot = new TelegramBot(process.env.BOT_API_KEY, {polling:true});
 var done = 0;
+var perPranzo = 0;
 var today_global = new Date();
 var dayOfWeek_global  = today_global.getDay();
 
@@ -24,13 +25,22 @@ bot.onText(/^[\/]{1}Start/, async (msg) => {
 
 bot.onText(/init/, async (msg) => {
     done = 0;
+    perPranzo = 0;
     console.log("init");
 });
 
 
 bot.onText(/mangiamo/, async (msg) => {
-    const quest = rispondi(questions.pranzo);
-    bot.sendMessage(msg.chat.id,quest);
+
+    if( GiornoCambiato()) console.log("cambiato Giorno");
+
+    if(perPranzo = 0) {
+        const quest = rispondi(questions.pranzo);
+        bot.sendMessage(msg.chat.id,quest);
+        perPranzo++;
+    } else {
+        bot.sendMessage(msg.chat.id,"per Oggi ho già risposto");
+    }
 });
 bot.onText(/ics/, async (msg) => {
     const quest = rispondi(questions.ics);
@@ -40,13 +50,21 @@ bot.onText(/Domandati/, async (msg) => {
     const quest = rispondi(questions.domandone);
     bot.sendMessage(msg.chat.id,quest);
 });
-
+function GiornoCambiato(){
+    var dayOfWeek = new Date().getDay();
+    if(dayOfWeek_global != dayOfWeek) {
+        dayOfWeek_global =  dayOfWeek;
+        done = 0;
+        perPranzo = 0;
+        return true;
+    }
+    return false;
+}
 function rispondi(lista){
     
-    var today = new Date();
-    var dayOfWeek = today.getDay();
-    if(dayOfWeek_global != dayOfWeek) done = 0;
-    var isFriday = (dayOfWeek === 5) ; 
+    if( GiornoCambiato()) console.log("cambiato Giorno");
+
+    var isFriday = (new Date().getDay() === 5) ; 
 
     if(isFriday && done < friday.esclamazioni.length) {     
          return friday.esclamazioni[done++] 
