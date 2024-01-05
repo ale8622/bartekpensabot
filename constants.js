798c8c7cf4966f17f4398bf617ca333c6b7fbc3d
Davide Zandonella <davide.zandonella@deltatre.com>
2024-01-05 16:01:52 +0100

    test

--- constants.js
+++ constants.js
@@ -2,5 +2,9 @@ const emoji = require("node-emoji");
 
 module.exports.Constants = {
  WelcomeMessage : "Cosa si starà chiedendo Bartek oggi?",
- Question : emoji.get('question')+"Domandati"+emoji.get('question')
+ Question : emoji.get('question')+" Domandati"+emoji.get('question'),
+ Lunch :emoji.get('spaghetti') +" Mangiamo"+emoji.get('question'),
+ Ics : emoji.get('x')+" ics",
+ Rigat : emoji.get('x')+" R.Dicecose",
+ FaiCose: emoji.get('x')+" sistema"
 }
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 require("./app")
 const { Constants } = require("./constants");
-const questions = require('./questions.json');
 const friday = require('./friday.json'); 
-
-
-const TelegramBot =require('node-telegram-bot-api')
+const questionsRedisKey = "questions";
+const questions_bck = require('./questions.json');
+const whats ="Cosa?";
+const TelegramBot =require('node-telegram-bot-api');
+const { setJson } = require("./redisClient");
 const bot = new TelegramBot(process.env.BOT_API_KEY, {polling:true});
+var id_message_start ="";
+var questions = "";
+const keyboard = {
+    "inline_keyboard": [
+        [
+            {"text": "Questions", "callback_data": "mangiamo?"},
+        ],
+        [
+            {"text": "Dove mangiare",  "callback_data": "Domandati?"},
+            {"text": "Ics", "callback_data": "ics"},
+        ]
+        ]
+    };
+
 var done = 0;
+var perPranzo = 0;
 var today_global = new Date();
 var dayOfWeek_global  = today_global.getDay();
-
+var menutoogle = true;
 bot.onText(/^[\/]{1}Start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, Constants.WelcomeMessage, {
@@ -26,6 +42,16 @@ bot.onText(/Domandati/, (msg) => {
     var today = new Date();
     var dayOfWeek = today.getDay();
     
+    console.log("Start from " + msg.from.username);
+
+    bot.sendMessage(msg.chat.id, Constants.WelcomeMessage, {
+        reply_markup : {
+            keyboard : [[Constants.Question],[Constants.Lunch],[Constants.Ics],[Constants.Rigat],],
+            force_reply : true
+        }
+    })
+
+
     if(dayOfWeek_global != dayOfWeek) done = 0;
     
 
@@ -41,3 +67,57 @@ bot.onText(/Domandati/, (msg) => {
     }
     
 });
+
+bot.onText(/sistema/, async (msg) => {
+    console.log("coseeee");
+     menutoogle = !menutoogle;
+ 
+     data = !menutoogle ? 
+         {"reply_to_message_id": msg.message_id, "force_reply" : "true"}
+         :{"reply_to_message_id": msg.message_id, "reply_markup": JSON.stringify(keyboard), "force_reply" : "true"};
+ 
+     bot.sendMessage(msg.chat.id, menutoogle ? "Menu Disattivato": "Menu Attivato", data , function (isSuccess) {
+         console.log(isSuccess);
+     });
+ 
+     return;
+     
+ });
+ 
+  bot.onText(/R.Dicecose/, async (msg) => {
+    
+     if(questions && questions.Rigat) {
+         var quest = rispondi(questions.Rigat);
+         bot.sendMessage("R. dice: " +  msg.chat.id,quest);
+     } else {
+         bot.sendMessage(msg.chat.id, whats);
+     }
+ });
+ 
+ bot.onText(/Mangiamo/, async (msg) => {
+     await setMessageForUser(msg);
+     if( GiornoCambiato()) console.log("cambiato Giorno");
+     if(perPranzo <= 0) {
+         if(questions && questions.pranzo) {
+             var quest = rispondi(questions.pranzo);
+             bot.sendMessage(msg.chat.id, "Oggi Mangerai da \n" + quest);
+         } else {
+             bot.sendMessage(msg.chat.id, whats);
+         }
+         
+         perPranzo++;
+     } else {
+         bot.sendMessage(msg.chat.id, msg.from.first_name + ", per Oggi ho già risposto");
+     }
+ });
+ 
+ bot.onText(/ics/, async (msg) => {
+     await setMessageForUser(msg);
+     if(questions && questions.domandone) {
+         var quest = questions.ics.map(x=> x + " \n");
+ 
+         bot.sendMessage(msg.chat.id, "Questi sono ICS: \n" +quest);
+     } else {
+         bot.sendMessage(msg.chat.id, whats);
+     }
+ });
